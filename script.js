cc62232abeac9d02f94e1373429284b68c5a3ac5
everysoularemine <danil.vishnyak.0505@gmail.com>
2024-10-09 11:38:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "everysoularemine❤";
+const neonText = document.getElementById("neon-text");
+
+text.split('').forEach((char, index) => {
+    const span = document.createElement('span');
+    span.className = 'letter';
+    span.textContent = char;
+    neonText.appendChild(span);
+    
+    // Задержка анимации для каждой буквы
+    span.style.animation = `draw 0.5s forwards ${index * 150}ms`;
+});
